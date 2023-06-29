cd1961b7810befb229b7a39748038cbd13e0822d
kburre <k.burre@eagles.oc.edu>
2023-06-29 16:58:47 -0500

    Home page added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,10 +2,10 @@
 import { onMounted } from "vue";
 import { ref, toRaw } from "vue";
 import { useRouter } from "vue-router";
-import UserServices from "../services/UserServices.js";
+import logo from "/icon.svg";
 
 const router = useRouter();
-const isCreateAccount = ref(false);
+
 const snackbar = ref({
   value: false,
   color: "",
@@ -25,44 +25,34 @@ function closeSnackBar() {
 <template>
   <v-container>
     <div id="body">
-      <v-card class="rounded-lg elevation-5">
-        <v-card-title class="headline mb-2">Login </v-card-title>
-        <v-card-text>
-          <v-text-field v-model="user.email" label="Email" required></v-text-field>
-
-          <v-text-field v-model="user.password" label="Password" type="password" required></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn variant="flat" color="secondary" >Create Account</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn variant="flat" color="primary">Login</v-btn>
-        </v-card-actions>
-      </v-card>
 
 
-      <v-dialog persistent v-model="isCreateAccount" width="800">
-        <v-card class="rounded-lg elevation-5">
-          <v-card-title class="headline mb-2">Create Account </v-card-title>
-          <v-card-text>
-            <v-text-field v-model="user.firstName" label="First Name" required></v-text-field>
+      <v-row>
+          <v-col cols="12" md="6">
+            <v-card-title class="headline mb-2">Welcome to Courier delivery system</v-card-title>
+            <v-card-text>
+              <p>
+                Courier is a courier service that allows you to send and receive
+                packages from anywhere in the world.
+              </p>
+              <p>
+                Courier is a courier service that allows you to send and receive
+                packages from anywhere in the world.
+              </p>
+              <p>
+                Courier is a courier service that allows you to send and receive
+                packages from anywhere in the world.
+              </p>
+            </v-card-text>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-img :src="logo"></v-img>
+          </v-col>
+        </v-row>
 
-            <v-text-field v-model="user.lastName" label="Last Name" required></v-text-field>
-
-            <v-text-field v-model="user.email" label="Email" required></v-text-field>
-
-            <v-text-field v-model="user.password" label="Password" type="password" required></v-text-field>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn variant="flat" color="secondary" >Close</v-btn>
-            <v-btn variant="flat" color="primary" >Create Account</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
 
       <v-snackbar v-model="snackbar.value" rounded="pill">
         {{ snackbar.text }}
-
         <template v-slot:actions>
           <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
             Close
