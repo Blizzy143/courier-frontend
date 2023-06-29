bb1d917667362bf533a7c16839209b56f2c0522f
MohanVamsi6484 <mohanvamsi.seelam@eagles.oc.edu>
2023-06-29 17:05:54 -0500

    Added authenticationi UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,9 +12,44 @@ const snackbar = ref({
   text: "",
 });
 
+const isLogin = ref(false);
+
+const user = ref({
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  number: "",
+});
+
 onMounted(async () => {
   
 });
+
+
+async function login() {
+  snackbar.value.value = true;
+  snackbar.value.color = "success";
+  snackbar.value.text = "Login successful";
+  closeLogin();
+  navigateToAdmin();
+}
+
+
+async function openLogin() {
+  isLogin.value = true;
+}
+
+async function closeLogin() {
+  isLogin.value = false;
+}
+
+async function navigateToAdmin() {
+  // router.push({ name: "admin" });
+  snackbar.value.value = true;
+  snackbar.value.color = "success";
+  snackbar.value.text = "Navigation to admin page successful";
+}
 
 
 function closeSnackBar() {
@@ -25,6 +60,12 @@ function closeSnackBar() {
 <template>
   <v-container>
     <div id="body">
+
+      <v-app-bar color="primary" app dark>
+        <!-- -->
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" @click="openLogin()">Login</v-btn>
+      </v-app-bar>
 
 
       <v-row>
@@ -50,6 +91,23 @@ function closeSnackBar() {
           </v-col>
         </v-row>
 
+
+
+      <v-dialog  v-model="isLogin" width="800">
+        <v-card class="rounded-lg elevation-5">
+          <v-card-title class="headline mb-2">Login </v-card-title>
+          <v-card-text>
+            <v-text-field v-model="user.email" label="Email" required></v-text-field>
+
+            <v-text-field v-model="user.password" label="Password" type="password" required></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn variant="flat" @click="closeLogin" color="secondary">Close</v-btn>
+            <v-btn variant="flat" @click="login" color="primary">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
       <v-snackbar v-model="snackbar.value" rounded="pill">
         {{ snackbar.text }}
