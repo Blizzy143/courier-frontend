f7eaccc43d61eb1527f7ad62c6fa40758c98da8c
subbarayala <rohith.subbarayala@eagles.oc.edu>
2023-07-10 04:22:34 -0500

    Custiomer list, Add, Edit, delete added

--- src/services/AdminServices.js
+++ src/services/AdminServices.js
@@ -6,5 +6,19 @@ export default {
   },
   updateCompanyDetails(company) {
     return apiClient.put("companies/" + company.id, company);
+  },
+  addCustomer(customer) {
+    return apiClient.post("customers", customer);
+  },
+  getCustomers() {
+    return apiClient.get("customers");
+  },
+  deleteCustomer(id) {
+    return apiClient.delete("customers/" + id);
+  },
+  updateCustomer(customer) { 
+    return apiClient.put("customers/" + customer.id, customer);
   }
+  
+
 };
